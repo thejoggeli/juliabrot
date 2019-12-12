bd9327668ce3145191bce565bbe60f9e609f9dc3
Tony <chuksi@github.com>
2019-12-12 02:50:24 -0800

    doxygen MainWindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -10,46 +10,84 @@
 class JuliaWidget;
 class JuliaRenderer;
 
-namespace Ui {
+namespace Ui
+{
 class MainWindow;
 }
+//! \brief The MainWindow class  Mainwindow include Qmainwindow
+class MainWindow : public QMainWindow
+{
+  Q_OBJECT
 
-class MainWindow : public QMainWindow {
-	Q_OBJECT
+ public:
+  //! \brief MainWindow default construct
+  //! \param parent pointer as param
+  explicit MainWindow(QWidget* parent = nullptr);
+  //! \brief getRenderTarget pointer refered to class JuliaWidget
+  //! \return type JuliaWidget
+  JuliaWidget* getRenderTarget();
+  //! \brief getFPS pointer refered to class QLabel
+  //! \return type getFPS
+  QLabel* getFPS();
+  //! \brief getValue constructor
+  //! \return returning integer
+  int getValue();
+  //! \brief getImaginary constructor
+  //! \return returning double
+  double getImaginary();
+  //! \brief getReal construct
+  //! \return returning double
+  double getReal();
+  //! \brief getRenderingMode constructor
+  //! \return returning integer
+  int getRenderingMode();
+  //! \brief getColorMode constructor
+  //! \return returning integer
+  int getColorMode();
+  //! \brief setStatusText constructor
+  //! \param text param defined as reference
+  void setStatusText(QString& text);
+  //! \brief getQuality constructor
+  //! \return returning double
+  double getQuality();
+  //! \brief resized bool param initialised as false
+  bool resized = false;
+  //! \brief exitRequested bool param initialised as false
+  bool exitRequested = false;
+  //! \brief juliaRenderer pointer with class JuliaRenderer
+  JuliaRenderer* juliaRenderer;
 
-public:
-	explicit MainWindow(QWidget* parent = nullptr);
-	JuliaWidget* getRenderTarget();
-	QLabel* getFPS();
-	int getValue();
-	double getImaginary();
-	double getReal();
-	int getRenderingMode();
-	int getColorMode();
-	void setStatusText(QString& text);
-	double getQuality();
-	bool resized = false;
-	bool exitRequested = false;
-	JuliaRenderer* juliaRenderer;
+  ~MainWindow();
 
-	~MainWindow();
+ protected:
+  //! \brief savePng constructor
+  //! \param w integer param
+  //! \param h integer param
+  void savePng(int w, int h);
 
-protected:
+ private slots:
+  //! \brief on_comboBox_currentIndexChanged constructor
+  //! \param index integer param
+  void on_comboBox_currentIndexChanged(int index);
+  //! \brief on_actionSave_PNG_Custom_triggered constructor
+  void on_actionSave_PNG_Custom_triggered();
+  //! \brief on_actionSave_PNG_1080_triggered constructor
+  void on_actionSave_PNG_1080_triggered();
+  //! \brief on_actionSave_PNG_2160_triggered constructor
+  void on_actionSave_PNG_2160_triggered();
+  //! \brief on_actionSave_PNG_4320_triggered constructor
+  void on_actionSave_PNG_4320_triggered();
+  //! \brief on_actionSave_PNG_8640_triggered constructor
+  void on_actionSave_PNG_8640_triggered();
+  //! \brief on_actionExit_triggered constructor
+  void on_actionExit_triggered();
 
-	void savePng(int w, int h);
-
-private slots:
-	void on_comboBox_currentIndexChanged(int index);
-	void on_actionSave_PNG_Custom_triggered();
-	void on_actionSave_PNG_1080_triggered();
-	void on_actionSave_PNG_2160_triggered();
-	void on_actionSave_PNG_4320_triggered();
-	void on_actionSave_PNG_8640_triggered();
-	void on_actionExit_triggered();
-
-private:
-	void resizeEvent(QResizeEvent* event);
-	Ui::MainWindow* ui;
+ private:
+  //! \brief resizeEvent constructor
+  //! \param event pointer with Qresizeevent as type
+  void resizeEvent(QResizeEvent* event);
+  //! \brief ui pointer param
+  Ui::MainWindow* ui;
 };
 
-#endif // MAINWINDOW_H
+#endif  // MAINWINDOW_H
